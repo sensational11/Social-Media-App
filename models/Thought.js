65b52dad83af1a027174b021416865e31b593220
sensational.mack <mackn478@gmail.com>
2022-11-19 22:08:30 -0500

    update route

--- models/Thought.js
+++ models/Thought.js
@@ -17,7 +17,7 @@ const thoughtSchema = new Schema({
     type: String,
     required: true,
   },
-reaction: [reactionSchema]
+// reaction: [reactionSchema]
 
 },{
     
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -13,7 +13,7 @@ router
 .get(getUsers).post(createUser);
 
 router
-.route("/:id")
+.route("/:userid")
 .get(getSingleUser);
 
 module.exports = router;
