47f00c243cccb33fe6e7a8c38dd097a1d7719700
sensational.mack <mackn478@gmail.com>
2022-11-19 20:48:00 -0500

    update usercontroller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,53 +1,24 @@
-const { ObjectId } = require("mongoose").Types;
-const { User, Thought } = require("../models");
+const { User } = require("../models");
 
-const userController = {
-  //Get all users
-  getUsers (req, res)  {
+module.exports = {
+  getUsers(req, res) {
     User.find()
-.then((users) => res.json(users))
-.catch((err) => res.status(500).json(err))
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
   },
-  // Get a single user
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
-      .select("-__v")
-      .then(async (user) =>
+      .select("-__V")
+      .then((user) =>
         !user
-          ? res.status(404).json({ mesage: "No user with that ID" })
-          : res.json({
-              user,
-              grade: await grade(req.params.userId),
-            })
+          ? res.status(400).json({ message: "No user with that ID" })
+          : res.json(user)
       )
-      .catch((err) => {
-        console.log(err);
-        return res.status(500).json(err);
-      });
+      .catch((err) => res.status(500).json(err));
   },
-
-  createUser({ body }, res) {
-    User.create(body)
+  createUser(req, res) {
+    User.create(req.body)
       .then((dbUserData) => res.json(dbUserData))
-      .catch((err) => {
-        console.log(err);
-        res.status(400).json(err);
-      });
-  },
-  updateUser({ params, body }, res) {
-    User.findOneAndUpdate({ _id: params.id }, body, {
-      new: true,
-      runValidators: true,
-    })
-      .then((dbUserData) => {
-        if (!dbUserData) {
-          res.status(404).json({ message: "There is no user with that ID" });
-          return;
-        }
-        res.json(dbUserData);
-      })
-      .catch((err) => res.status(400).json(err));
+      .catch((err) => res.status(500).json(err));
   },
 };
-
-module.exports = userController;
